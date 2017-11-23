7da2781eb2fb9b2297936717a7fcf1facdb60659
Jacopo Penazzi <j.penazzi@cineca.it>
2017-11-23 17:51:08 +0100

    browsersync reload fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
+require('dotenv').config();
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
+var b2v = require('buffer-to-vinyl');
 var source = require('vinyl-source-stream');
 var sourceMaps = require('gulp-sourcemaps');
 var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
-//var  = require('gulp-');
 var del = require('del');
 var runSequence = require('run-sequence');
+var ngConfig = require('gulp-ng-config');
 
+var ENV = process.env.APP_ENV || 'development';
 
 /* TASK TEMPLATE
 
@@ -38,8 +42,24 @@ var CONFIG = {
         'js/**/*',
         'flag-icon-css/**/*.*',
         'font-awesome/**/*.*',
-    ]
+    ],
+    partials: {
+        sources : [
+            'src/**/*.html',
+            'src/**/*.json',
+            '!src/index.html'
+        ]
+    }
 }
+
+gulp.task('ng-config', function() {
+    var config = require('./config/config.js');
+    var json = JSON.stringify(config[ENV]);
+  
+    return b2v.stream(new Buffer(json), 'app.constants.js')
+      .pipe(ngConfig('app.constants', { constants: config[ENV], createModule:true }))
+      .pipe(gulp.dest('src/app'));
+  });
 
 
 gulp.task('sass', function () {
@@ -49,17 +69,6 @@ gulp.task('sass', function () {
         .pipe(browserSync.reload({
             stream: true
         }))
-})
-
-gulp.task('watch', ['browserSync', 'sass', 'browserify'], function () {
-    gulp.watch('src/assets/scss/**/*.scss', ['sass']);
-    gulp.watch('src/*.html', browserSync.reload);
-    //   gulp.watch('src/**/*.js', browserSync.reload);
-    gulp.watch('src/**/*.js', ['browserify'], function () {
-
-        browserSync.reload({ stream: true });
-    });
-
 })
 
 gulp.task('browserSync', function () {
@@ -133,20 +142,8 @@ gulp.task('cache:clear', function (callback) {
     // return cache.clearAll(callback);
 })
 
-gulp.task('build', function (callback) {
-    runSequence('clean:build', ['sass', 'useref', 'images', 'fonts', 'assets', 'partials', 'browserify'], callback);
-})
-
-gulp.task('default', function (callback) {
-    runSequence(['sass', 'partials', 'browserify', 'browserSync', 'watch'], callback);
-})
-
-gulp.task('assets', function() {
-    return gulp.src(CONFIG.assets, { base: 'src/assets', cwd: 'src/assets/**' })
-        .pipe(gulp.dest('build/assets'));
-})
-
 gulp.task('partials', function() {
+    console.log("eseguo task partials");
     var sources = [
                 'src/**/*.html',
                 'src/**/*.json',
@@ -155,3 +152,39 @@ gulp.task('partials', function() {
     return gulp.src( sources )
         .pipe(gulp.dest('build'));
 })
+
+gulp.task('assets', function() {
+    return gulp.src(CONFIG.assets, { base: 'src/assets', cwd: 'src/assets/**' })
+        .pipe(gulp.dest('build/assets'));
+})
+
+
+gulp.task('build', function (callback) {
+    runSequence('clean:build', [ 'ng-config', 'sass', 'useref', 'images', 'fonts', 'assets', 'partials', 'browserify'], callback);
+})
+
+gulp.task('default', function (callback) {
+    runSequence([ 'ng-config', 'sass', 'partials', 'browserify', 'browserSync', 'watch'], callback);
+})
+
+gulp.task('watch', [ 'sass', 'browserify', 'browserSync' ], function () {
+    gulp.watch('src/assets/scss/**/*.scss', ['sass'], function() {
+        browserSync.reload({ stream: true });
+    });
+    gulp.watch('src/*.html', ['useref'], function() {
+        browserSync.reload({ stream: true });
+    });
+    gulp.watch('src/assets/fonts/**/*.*', ['fonts'], function() {
+        browserSync.reload({ stream: true });
+    })
+    gulp.watch( ['src/**/*.html', 'src/**/*.json','!src/index.html'], function() {
+        runSequence(['partials'], browserSync.reload);
+    })
+    gulp.watch('src/**/*.js', ['browserify'], function () {
+
+        browserSync.reload({ stream: true });
+    });
+
+})
+
+gulp.task('browser-reload', function() {browserSync.reload({stream:true})});
